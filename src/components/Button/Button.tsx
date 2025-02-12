612990e6ea830f2bfa09b6c96073745131509d0b
Maksim <makys1703@yandex.ru>
2025-02-12 04:56:41 +0300

    homework-3.2

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,8 @@
-import { DetailedHTMLProps, ButtonHTMLAttributes, MouseEventHandler } from 'react';
+import { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.css';
 
 interface Props extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   text: string,
-  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
 export function Button({ text, ...props }: Props) {
